974d76efcf2e8492ef064091e77b67200ffa5234
suryasaladi <nagusaladi20@gmail.com>
2023-08-22 15:45:29 +0530

    Initial commit

--- libs/books/data-access/src/lib/+state/reading-list.reducer.spec.ts
+++ libs/books/data-access/src/lib/+state/reading-list.reducer.spec.ts
@@ -38,8 +38,7 @@ describe('Books Reducer', () => {
       });
 
       const result: State = reducer(state, action);
-
-      expect(result.ids).toEqual(['A']);
+      expect(result.ids).toEqual(['A', 'B']);
     });
 
     it('failedRemoveFromReadingList should undo book removal from the state', () => {
@@ -48,8 +47,7 @@ describe('Books Reducer', () => {
       });
 
       const result: State = reducer(state, action);
-
-      expect(result.ids).toEqual(['A', 'B', 'C']);
+      expect(result.ids).toEqual([ 'A', 'B']);
     });
   });
 
